9151084b66f0e470ec9302277bf4626ae4cced77
Easterlings <1299171412@qq.com>
2023-03-16 21:02:24 +0800

    hw7

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -1,11 +1,35 @@
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s) (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s) (car (cdr (cdr s))))
 
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst) 
+    (
+        if(> (length asc-lst) 1) 
+        (
+            if(> (car asc-lst) (cadr asc-lst)) 
+                false
+                (ascending? (cdr asc-lst))
+        )
+        true
+    )
+)
 
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+    (
+        if (= base 1)
+        1
+        (
+            if(= exp 1)
+            base
+            (
+                if (= (modulo exp 2) 0)
+                    (square (pow base (/ exp 2)))
+                    (* base (square (pow base (/ (- exp 1) 2))))
+            )
+        )
+    )
+)
