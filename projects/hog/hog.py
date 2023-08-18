2456e23f5ee8b2ebbc38bd539dd170a4d301981f
Easterlings <1299171412@qq.com>
2023-08-18 17:35:08 +0800

    p1Q5

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -22,7 +22,6 @@ def roll_dice(num_rolls, dice=six_sided):
     assert type(num_rolls) == int, 'num_rolls must be an integer.'
     assert num_rolls > 0, 'Must roll at least once.'
     # BEGIN PROBLEM 1
-    "*** YOUR CODE HERE ***"
     count = 0
     one = 0
     for i in range(num_rolls):
@@ -30,7 +29,12 @@ def roll_dice(num_rolls, dice=six_sided):
         if dice_thistime == 1:
             one = 1
         count = count + dice_thistime
-    return 1 if one == 1 else count
+    res = (1 if one == 1 else count)
+    # if(count ==3):
+    # print(one)
+    # print(count)
+    # print(res)
+    return res
     # END PROBLEM 1
 
 
@@ -41,7 +45,9 @@ def tail_points(opponent_score):
 
     """
     # BEGIN PROBLEM 2
-    "*** YOUR CODE HERE ***"
+    ones = opponent_score%10
+    tens = opponent_score%100//10
+    return 2*abs(tens-ones)+1
     # END PROBLEM 2
 
 
@@ -58,7 +64,11 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return tail_points(opponent_score)
+    point = roll_dice(num_rolls, dice)
+    opponent_point = tail_points(opponent_score)
+    return point if point>opponent_point else opponent_point
     # END PROBLEM 3
 
 
@@ -66,7 +76,9 @@ def simple_update(num_rolls, player_score, opponent_score, dice=six_sided):
     """Return the total score of a player who starts their turn with
     PLAYER_SCORE and then rolls NUM_ROLLS DICE, ignoring Square Swine.
     """
-    return player_score + take_turn(num_rolls, opponent_score, dice)
+    add = take_turn(num_rolls, opponent_score, dice)
+    # print("add:",add)
+    return player_score + add
 
 
 def square_update(num_rolls, player_score, opponent_score, dice=six_sided):
@@ -81,7 +93,11 @@ def square_update(num_rolls, player_score, opponent_score, dice=six_sided):
 
 
 # BEGIN PROBLEM 4
-"*** YOUR CODE HERE ***"
+def perfect_square(score):
+    return score ** 0.5 % 1 == 0
+ 
+def next_perfect_square(score):
+    return int((score ** 0.5 + 1)**2)
 # END PROBLEM 4
 
 
@@ -120,7 +136,12 @@ def play(strategy0, strategy1, update,
     """
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
-    "*** YOUR CODE HERE ***"
+    while score0 < goal and score1 < goal:
+        if who == 0:
+            score0 = update(strategy0(score0, score1), score0, score1, dice)
+        else:
+            score1 = update(strategy1(score1, score0), score1, score0, dice)
+        who = 1 - who
     # END PROBLEM 5
     return score0, score1
 
