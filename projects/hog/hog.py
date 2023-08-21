803f287ccd3c61ca606775ac3c53b17ebae678be
Easterlings <1299171412@qq.com>
2023-08-21 08:16:50 +0800

    on

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -64,11 +64,10 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
-    if num_rolls == 0:
+    if num_rolls==0:
         return tail_points(opponent_score)
-    point = roll_dice(num_rolls, dice)
-    opponent_point = tail_points(opponent_score)
-    return point if point>opponent_point else opponent_point
+    else:
+        return roll_dice(num_rolls,dice)
     # END PROBLEM 3
 
 
